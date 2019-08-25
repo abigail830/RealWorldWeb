999ca2ca6862682e37a9c773b9e993042834f0ae
SaraQian <abigail830@163.com>
2019-08-25 15:19:49 +0800

    hardcode step done

---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+    <nav class="navbar navbar-light">
+      <div class="container">
+        <a class="navbar-brand" href="index.html">conduit</a>
+        <ul class="nav navbar-nav pull-xs-right">
+          <li class="nav-item">
+            <!-- Add "active" class when you're on that page" -->
+            <router-link to="/" class="nav-link">Home</router-link>
+          </li>
+          <li class="nav-item">
+              <router-link to="/create-article" class="nav-link">New Post</router-link>
+          </li>
+          <li class="nav-item">
+              <router-link to="/settings" class="nav-link">Settings</router-link>
+          </li>
+          
+          <li class="nav-item">
+              <router-link to="/login" class="nav-link">Sign up</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+</template>
--- src/router.ts
+++ src/router.ts
@@ -14,12 +14,34 @@ export default new Router({
       component: Home,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      path: '/login',
+      name: 'login',
+      component: () => import('./views/Login.vue'),
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('./views/Register.vue'),
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: () => import('./views/Profile.vue'),
+    },
+    {
+      path: '/article',
+      name: 'article',
+      component: () => import('./views/Article.vue'),
+    },
+    {
+      path: '/create-article',
+      name: 'create-article',
+      component: () => import('./views/CreateArticle.vue'),
+    },
+    {
+      path: '/settings',
+      name: 'settings',
+      component: () => import('./views/Settings.vue'),
     },
   ],
 });
